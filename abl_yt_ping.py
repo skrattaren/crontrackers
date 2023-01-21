8e08cceb783067426e7d9dc5456418cbbfe72f11
Nikolaj Šujskij <skrattaren@yandex.ru>
2023-01-21 13:37:46 +0400

    Log stuff properly

--- abl_yt_ping.py
+++ abl_yt_ping.py
@@ -2,14 +2,16 @@
 
 import argparse
 import json
+import logging
 import os
 import re
 import sys
 
 import requests
+import lxml
 from lxml.html import soupparser as html
-# from lxml import etree
 
+SCRIPT_NAME = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 BASE_URL = 'https://ablforpeople.com/game'
 MEDIA_SUFFIX = 'media'
 GAME_URL = '%s/{}/%s' % (BASE_URL, MEDIA_SUFFIX)
@@ -18,12 +20,11 @@ NTFY_TOPIC = 'skrattaren-ntfy'
 
 _STATE_DIR = os.path.join(os.getenv('HOME', '.'), '.local', 'state')
 _STATE_DIR = os.getenv('XDG_STATE_HOME', _STATE_DIR)
-STATE_FILE = os.path.join(_STATE_DIR, 'abl_yt_ping.json')
+STATE_FILE = os.path.join(_STATE_DIR, f'{SCRIPT_NAME}.json')
 
-
-def err(msg, exit_code=1):
-    print(msg, file=sys.stderr)
-    sys.exit(exit_code)
+LOGGER = logging.Logger(SCRIPT_NAME)
+LOGGER.setLevel('WARN')
+LOGGER.addHandler(logging.StreamHandler())
 
 
 def notify(url):
@@ -46,26 +47,37 @@ def parse_args():
     parser.add_argument('-c', '--no-cache', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
+    if args.verbose:
+        LOGGER.setLevel('INFO')
+        LOGGER.info("Entering verbose mode")
     if args.id_or_url.isdigit():
         args.game_id = args.id_or_url
     elif arg_match := re.match(URL_RE, args.id_or_url):
         args.game_id = arg_match.groups()[0]
     else:
-        err(f"Unable to parse argument: {args.id_or_url}")
+        LOGGER.critical("Unable to parse argument: %s", args.id_or_url)
+        sys.exit(1)
     args.game_url = GAME_URL.format(args.game_id)
+    args.game_id = int(args.game_id)
+    LOGGER.info("Game ID: %s", args.game_id)
+    LOGGER.info("Game URL: %s", args.game_url)
     return args
 
 
 def check_cache(game_id):
+    LOGGER.info("Trying to use '%s' as state file", STATE_FILE)
     games = []
-    game_id = int(game_id)
     if os.path.isfile(STATE_FILE):
         with open(STATE_FILE, 'r', encoding='utf-8') as state_file:
             games = json.load(state_file)
+        LOGGER.info("List of game IDs loaded: %s", games)
+    else:
+        LOGGER.info("No state file found")
     if game_id in games:
-        print(f"Game ID {game_id} found in cache, exiting")
+        LOGGER.info("Game ID %s found in cache, exiting", game_id)
         sys.exit(0)
     games.append(game_id)
+    LOGGER.info("Saving games to state file: %s", games)
     with open(STATE_FILE, 'w', encoding='utf-8') as state_file:
         json.dump(games, state_file)
 
@@ -75,15 +87,16 @@ def main():
     r = requests.get(args.game_url, timeout=13)
     doc = html.fromstring(r.text)
     video_elem = doc.find('body//div[@class="video-records"]/div/iframe')
+    LOGGER.info("Found video <iframe>:\n%s",
+                lxml.html.tostring(video_elem).decode('utf-8'))
     if video_elem is None:
         return
-    if not args.no_cache:
-        check_cache(args.game_id)
     yt_url = video_elem.attrib['src']
     yt_url = yt_url.replace('embed/', 'watch?v=')
-    if args.no_notification:
-        print(f"YouTube link found: {yt_url}")
-    else:
+    LOGGER.info("YouTube link found: %s", yt_url)
+    if not args.no_cache:
+        check_cache(args.game_id)
+    if not args.no_notification:
         notify(yt_url)
 
 
